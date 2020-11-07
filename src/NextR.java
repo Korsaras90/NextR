870edfaf34741d86420689b2c7a896ebd5f81b6f
Korsaras90 <sergbirukof@gmail.com>
2020-11-07 19:52:44 +0300

    Initial commit

---
+++ src/NextR.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class NextR {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int vol = in.nextInt();
+        int ball = in.nextInt();
+
+        System.out.println(vol + " " + ball);
+    }
+}
+
